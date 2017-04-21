08eaecccd1ae85bdc17bd8535ad0e2e9fb3e42b5
daoduytung2209 <daoduytung2209@gmail.com>
2017-04-22 00:40:44 +0800

    Create a new file

---
+++ DuyTungDaoA1.py
@@ -0,0 +1,11 @@
+# Name: Dao Duy Tung
+# Date: 21/4/2017
+# Brief program details: This program is a simple shopping list that could
+# also be used for TODO lists or similar purposes.
+# The program maintains a list of items in a file, and each item has:
+# •	name, price, priority (1, 2 or 3), whether it is required or completed
+# Users can choose to see the list of required items or completed items, including a total of the (estimated) price of
+# all of those items. The lists will be sorted by priority.
+# Users can add new items and mark items as completed.
+# They cannot change items from completed to required.
+# Github project link: https://github.com/daoduytung2209/A1
