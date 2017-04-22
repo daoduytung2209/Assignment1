ff10cc2838d01029c7ce7badc8634038e175c46c
daoduytung2209 <daoduytung2209@gmail.com>
2017-04-22 10:38:28 +0800

    Write pseudocode and code for load items function

--- DuyTungDaoA1.py
+++ DuyTungDaoA1.py
@@ -9,3 +9,23 @@
 # Users can add new items and mark items as completed.
 # They cannot change items from completed to required.
 # Github project link: https://github.com/daoduytung2209/A1
+
+
+# Pseudocode for load_items function:
+# Open items.csv and read the content
+# Initialize for loop that accesses each line in the file
+# Replace "\n" by "" in each line
+# Split each line in the file by the comma and add to list_data
+# Close the file
+# Return list_data
+
+def load_items(filename):
+    list_data = []
+    input_file = open(filename, "r")
+    for each in input_file:
+        if "\n" in each:
+            each = each.replace("\n", "")
+        split_data = each.split(",")
+        list_data.append(split_data)
+    input_file.close()
+    return list_data
